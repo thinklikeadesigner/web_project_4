3cf10014be6250bab13caac025ed29378ab2edb3
rebecca <rebecca.burch.stu@gmail.com>
2020-07-21 00:27:00 -0500

    refactored js

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
-console.log("hello");
 const pencil = document.querySelector(".edit");
 const closeButton = document.querySelector(".popUp__close-button");
+const submitButton = document.querySelector(".form__button");
+
 const popUp = document.querySelector(".popUp");
 const form = document.querySelector(".form");
+
 const nameInput = document.querySelector(".form__input_type_name");
 const jobInput = document.querySelector(".form__input_type_info");
+
 const profileName = document.querySelector(".profile__name");
 const profileJob = document.querySelector(".profile__job");
-console.log("1");
-const submitButton = document.querySelector(".form__button");
-console.log("2");
 
 function togglePopUp() {
   popUp.classList.toggle("popUp_open");
-  console.log("toggleEditPopUp");
-  return;
 }
-
-pencil.addEventListener("click", togglePopUp);
-console.log("3");
-closeButton.addEventListener("click", togglePopUp);
 
 function formSubmitHandler(e) {
   e.preventDefault();
-
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-
   togglePopUp();
-  return;
 }
 
+pencil.addEventListener("click", togglePopUp);
+closeButton.addEventListener("click", togglePopUp);
 form.addEventListener("submit", formSubmitHandler);
