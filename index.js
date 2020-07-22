38cd99d4e211bb91ed94677d207fac65006153a1
rebecca <rebecca.burch.stu@gmail.com>
2020-07-21 19:32:52 -0500

    fix: CR1, double checking css

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const pencil = document.querySelector(".profile__edit");
-const closeButton = document.querySelector(".popUp__close-button");
-const submitButton = document.querySelector(".form__button");
+const editButton = document.querySelector(".profile__edit");
+const closeButton = document.querySelector(".popup__close-button");
 
-const popUp = document.querySelector(".popUp");
+const popup = document.querySelector(".popup");
 const form = document.querySelector(".form");
 
 const nameInput = document.querySelector(".form__input_type_name");
@@ -11,17 +10,17 @@ const jobInput = document.querySelector(".form__input_type_info");
 const profileName = document.querySelector(".profile__name");
 const profileJob = document.querySelector(".profile__job");
 
-function togglePopUp() {
-  popUp.classList.toggle("popUp_open");
+function togglepopup() {
+  popup.classList.toggle("popup_open");
 }
 
 function formSubmitHandler(e) {
   e.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  togglePopUp();
+  togglepopup();
 }
 
-pencil.addEventListener("click", togglePopUp);
-closeButton.addEventListener("click", togglePopUp);
+editButton.addEventListener("click", togglepopup);
+closeButton.addEventListener("click", togglepopup);
 form.addEventListener("submit", formSubmitHandler);
