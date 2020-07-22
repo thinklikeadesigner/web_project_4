5cb72efe200ea33e046738212884a30abe686af1
rebecca <rebecca.burch.stu@gmail.com>
2020-07-22 00:27:53 -0500

    fix: reviewed changes

--- scripts/index.js
+++ scripts/index.js
@@ -10,17 +10,35 @@ const jobInput = document.querySelector(".form__input_type_info");
 const profileName = document.querySelector(".profile__name");
 const profileJob = document.querySelector(".profile__job");
 
-function togglepopup() {
-  popup.classList.toggle("popup_open");
+function openPopup() {
+  popup.classList.add("popup_open");
+}
+
+function closePopup() {
+  popup.classList.remove("popup_open");
+}
+
+function preFill() {
+  if ((popup["class"] = "popup_open")) {
+    console.log("opened popup");
+    nameInput.value = profileName.textContent;
+    console.log('changed value="" for name');
+    jobInput.value = profileJob.textContent;
+    console.log('changed value="" for job');
+  }
 }
 
 function formSubmitHandler(e) {
   e.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  togglepopup();
+  closePopup();
 }
 
-editButton.addEventListener("click", togglepopup);
-closeButton.addEventListener("click", togglepopup);
+editButton.addEventListener("click", openPopup);
+editButton.addEventListener("click", preFill);
+
+closeButton.addEventListener("click", closePopup);
+closeButton.addEventListener("click", preFill);
+
 form.addEventListener("submit", formSubmitHandler);
