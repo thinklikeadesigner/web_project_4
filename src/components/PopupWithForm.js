5d4c43d730d0ab8a51eaf844c29ef142d2388bb6
rebecca burch <rebecca.burch.stu@gmail.com>
2021-01-21 22:08:35 -0600

    finishing touches, form avatar reset

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -8,6 +8,7 @@ export default class PopupWithForm extends Popup {
     this._handleFormSubmit = handleFormSubmit;
     this._formEdit = document.querySelector(".form_edit");
     this._formAdd = document.querySelector(".form_add");
+    this._formAvatar = document.querySelector(".form_avatar");
     this._formDelete = document.querySelector(".modal_type_delete");
     
 
@@ -22,12 +23,12 @@ export default class PopupWithForm extends Popup {
     super.close();
     this._formEdit.reset();
     this._formAdd.reset();
+    this._formAvatar.reset();
   }
 
 
   _getInputValues() {
     this._inputList = this._popupElement.querySelectorAll(".form__input");
-
     this._formValues = {};
     this._inputList.forEach(
       (input) => (this._formValues[input.name] = input.value)
